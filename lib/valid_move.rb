4826c854d435ca13d089abd2504e4e0edcdab736
Sharnell Grant <sharnellgrant@gmail.com>
2019-07-07 18:40:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-if position_taken?(board, index) == valid_move?(0, 8)
+if position_taken?(board, index) != valid_move?(0, 8)
   return true
  end
 end 
